1516836fb3636dcd1232b8ceb6bb3c35c673f0e8
majdzaatri <majdz-1@live.com>
2020-06-15 19:02:11 +0300

    added gui

---
+++ src/sample/LoginDemoApplication.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/** Main application class for the login demo application */
+public class LoginDemoApplication extends Application {
+
+  public static void main(String[] args) { launch(args); }
+  @Override public void start(Stage stage) throws IOException {
+    Scene scene = new Scene(new StackPane());
+    
+    LoginManager loginManager = new LoginManager(scene);
+    loginManager.showLoginScreen();
+
+    stage.setScene(scene);
+    stage.show();
+  }
+}
